7a16b3b5cad18e572590a37ac495230bb918a289
TrinhXuanDong <info.dongtx@gmail.com>
2016-01-18 19:21:19 +0700

    Add form bao duong xe

---
+++ iSmartCar/iSmartCar/DanhMuc/DanhMucBaoDuongXe/frmBaoDuongXe.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace iSmartCar
+{
+    partial class frmBaoDuongXe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this._treeView = new System.Windows.Forms.TreeView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this._btnBoLocBienSoXe = new System.Windows.Forms.Button();
+            this._btnLocBienSoXe = new System.Windows.Forms.Button();
+            this._txtLocBienSoXe = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.myDataGridView1 = new iSmartCar.MyDataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.myDataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this._treeView);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(218, 448);
+            this.panel2.TabIndex = 1;
+            // 
+            // _treeView
+            // 
+            this._treeView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._treeView.Location = new System.Drawing.Point(0, 29);
+            this._treeView.Name = "_treeView";
+            this._treeView.Size = new System.Drawing.Size(216, 383);
+            this._treeView.TabIndex = 3;
+            this._treeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.OnTreeViewAfterSelect);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.75F);
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(216, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "DANH SÁCH XE";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this._btnBoLocBienSoXe);
+            this.panel4.Controls.Add(this._btnLocBienSoXe);
+            this.panel4.Controls.Add(this._txtLocBienSoXe);
+            this.panel4.Controls.Add(this.label3);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel4.Location = new System.Drawing.Point(0, 412);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(216, 34);
+            this.panel4.TabIndex = 1;
+            // 
+            // _btnBoLocBienSoXe
+            // 
+            this._btnBoLocBienSoXe.Image = global::iSmartCar.Properties.Resources.filter_delete_icon;
+            this._btnBoLocBienSoXe.Location = new System.Drawing.Point(169, 4);
+            this._btnBoLocBienSoXe.Margin = new System.Windows.Forms.Padding(4);
+            this._btnBoLocBienSoXe.Name = "_btnBoLocBienSoXe";
+            this._btnBoLocBienSoXe.Size = new System.Drawing.Size(41, 27);
+            this._btnBoLocBienSoXe.TabIndex = 2;
+            this._btnBoLocBienSoXe.UseVisualStyleBackColor = true;
+            this._btnBoLocBienSoXe.Click += new System.EventHandler(this.OnBtnBoLocBienSoXeMouseClick);
+            // 
+            // _btnLocBienSoXe
+            // 
+            this._btnLocBienSoXe.Image = global::iSmartCar.Properties.Resources.filter_add_icon;
+            this._btnLocBienSoXe.Location = new System.Drawing.Point(124, 4);
+            this._btnLocBienSoXe.Margin = new System.Windows.Forms.Padding(4);
+            this._btnLocBienSoXe.Name = "_btnLocBienSoXe";
+            this._btnLocBienSoXe.Size = new System.Drawing.Size(41, 27);
+            this._btnLocBienSoXe.TabIndex = 3;
+            this._btnLocBienSoXe.UseVisualStyleBackColor = true;
+            this._btnLocBienSoXe.Click += new System.EventHandler(this.OnBtnLocBienSoXeMouseClick);
+            // 
+            // _txtLocBienSoXe
+            // 
+            this._txtLocBienSoXe.Location = new System.Drawing.Point(45, 6);
+            this._txtLocBienSoXe.Name = "_txtLocBienSoXe";
+            this._txtLocBienSoXe.Size = new System.Drawing.Size(75, 23);
+            this._txtLocBienSoXe.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "B.Số:";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.myDataGridView1);
+            this.panel3.Controls.Add(this.panel1);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(218, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(732, 448);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 414);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(732, 34);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.75F);
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(732, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TÓM TẮT NỘI DUNG BẢO DƯỠNG CỦA BIỂN SỐ XE: ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Image = global::iSmartCar.Properties.Resources.XOA;
+            this.button4.Location = new System.Drawing.Point(680, 4);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(44, 27);
+            this.button4.TabIndex = 1;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Image = global::iSmartCar.Properties.Resources.edit_validated_icon;
+            this.button3.Location = new System.Drawing.Point(628, 4);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(44, 27);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Image = global::iSmartCar.Properties.Resources.Them;
+            this.button2.Location = new System.Drawing.Point(576, 4);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(44, 27);
+            this.button2.TabIndex = 3;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Image = global::iSmartCar.Properties.Resources.Import_icon;
+            this.button1.Location = new System.Drawing.Point(527, 4);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(41, 27);
+            this.button1.TabIndex = 4;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // myDataGridView1
+            // 
+            this.myDataGridView1.AllowUserToAddRows = false;
+            this.myDataGridView1.AllowUserToDeleteRows = false;
+            this.myDataGridView1.AllowUserToOrderColumns = true;
+            this.myDataGridView1.AllowUserToResizeRows = false;
+            this.myDataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.myDataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7});
+            this.myDataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.myDataGridView1.Location = new System.Drawing.Point(0, 29);
+            this.myDataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.myDataGridView1.MultiSelect = false;
+            this.myDataGridView1.Name = "myDataGridView1";
+            this.myDataGridView1.ReadOnly = true;
+            this.myDataGridView1.RowHeadersVisible = false;
+            this.myDataGridView1.Size = new System.Drawing.Size(732, 385);
+            this.myDataGridView1.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Mã bảo dưỡng";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Ngày";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 50;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Tháng";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 50;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Năm";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 50;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Km bảo dưỡng";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            this.Column5.Width = 200;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Tên xưởng máy";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            this.Column6.Width = 200;
+            // 
+            // Column7
+            // 
+            this.Column7.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column7.HeaderText = "Tổng tiền";
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            // 
+            // frmBaoDuongXe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(950, 448);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmBaoDuongXe";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bảo dưỡng xe";
+            this.panel2.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.myDataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TreeView _treeView;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox _txtLocBienSoXe;
+        private System.Windows.Forms.Button _btnBoLocBienSoXe;
+        private System.Windows.Forms.Button _btnLocBienSoXe;
+        private MyDataGridView myDataGridView1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+    }
+}
---
+++ iSmartCar/iSmartCar/DanhMuc/DanhMucBaoDuongXe/frmBaoDuongXe.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace iSmartCar
+{
+    public partial class frmBaoDuongXe : Form
+    {
+        #region Contructor
+        public frmBaoDuongXe()
+        {
+            InitializeComponent();
+
+            HandleCreated += OnHandleCreated;
+        }
+        #endregion
+
+        #region Event
+        private void OnHandleCreated(object sender, EventArgs e)
+        {
+            CreateTreeView();
+        }
+
+        private void OnTreeViewAfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node.Level == 1)
+            {
+                MessageBox.Show(e.Node.Text);
+            }
+        }
+
+        private void OnBtnBoLocBienSoXeMouseClick(object sender, EventArgs e)
+        {
+            CreateTreeView();
+        }
+
+        private void OnBtnLocBienSoXeMouseClick(object sender, EventArgs e)
+        {
+            if (_txtLocBienSoXe.Text != "")
+            {
+                string bienSoXe = _txtLocBienSoXe.Text;
+                CreateTreeView(bienSoXe);
+            }
+        }
+
+        #endregion
+
+        #region Method
+        /// <summary>
+        /// Hàm tạo danh sách xe theo dạng cây
+        /// </summary>
+        /// <param name="bienSoXe"></param>
+        private void CreateTreeView(string bienSoXe = "")
+        {
+            _treeView.Nodes.Clear();
+
+            //Lấy ra danh sách xe
+            string sql = "select * from Xe";
+            if (bienSoXe != "")
+            {
+                sql += " where BienSo like N'%" + bienSoXe + "%'";
+            }
+            DataTable dtb = GuiResInfoMng.Instance.GetDataTable(sql);
+
+            //Lấy ra các loại xe khác nhau
+            List<string> dSachLoaiXe = new List<string>();
+            for (int i = 0; i < dtb.Rows.Count; i++)
+            {
+                string loaiXe = dtb.Rows[i][1].ToString();
+                if (dSachLoaiXe.Contains(loaiXe) == false)
+                {
+                    dSachLoaiXe.Add(loaiXe);
+                }
+            }
+            //Thêm loại xe vào treeview với vai trò là root
+            for (int i = 0; i < dSachLoaiXe.Count; i++)
+            {
+                //Lấy ra danh sách biển số xe thuộc cùng 1 loại xe
+                sql = string.Format("select BienSo from Xe where LoaiXe = N'{0}'", dSachLoaiXe[i]);
+
+                if (bienSoXe != "")
+                {
+                    sql += " and  BienSo like N'%" + bienSoXe + "%'";
+                }
+
+                dtb = GuiResInfoMng.Instance.GetDataTable(sql);
+
+                List<TreeNode> childrenNode = new List<TreeNode>();
+                for (int j = 0; j < dtb.Rows.Count; j++)
+                {
+                    TreeNode tmp = new TreeNode(dtb.Rows[j][0].ToString());
+                    childrenNode.Add(tmp);
+                }
+
+                TreeNode root = new TreeNode(dSachLoaiXe[i] + " [" + dtb.Rows.Count + "]", childrenNode.ToArray());
+
+                _treeView.Nodes.Add(root);
+            }
+            _treeView.ExpandAll();
+        }
+        #endregion
+    }
+}
